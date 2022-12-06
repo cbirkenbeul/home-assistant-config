42ea82de68ebe7ae9d1b5828075f291593e1271f
Christian Birkenbeul <github@teqqy.de>
2022-12-06 23:29:16 +0100

    Update

--- custom_components/tesla_custom/binary_sensor.py
+++ custom_components/tesla_custom/binary_sensor.py
@@ -1,40 +1,245 @@
-"""Support for Tesla binary sensor."""
+"""Support for Tesla binary sensors."""
+import logging
 
-from homeassistant.components.binary_sensor import DEVICE_CLASSES, BinarySensorEntity
+from teslajsonpy.car import TeslaCar
+from teslajsonpy.const import GRID_ACTIVE, RESOURCE_TYPE_BATTERY
+from teslajsonpy.energy import PowerwallSite
 
-from . import DOMAIN as TESLA_DOMAIN
-from .tesla_device import TeslaDevice
+from homeassistant.components.binary_sensor import (
+    BinarySensorDeviceClass,
+    BinarySensorEntity,
+)
+from homeassistant.core import HomeAssistant
 
+from . import TeslaDataUpdateCoordinator
+from .base import TeslaCarEntity, TeslaEnergyEntity
+from .const import DOMAIN
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up the Tesla binary_sensors by config_entry."""
-    async_add_entities(
-        [
-            TeslaBinarySensor(
-                device,
-                hass.data[TESLA_DOMAIN][config_entry.entry_id]["coordinator"],
-            )
-            for device in hass.data[TESLA_DOMAIN][config_entry.entry_id]["devices"][
-                "binary_sensor"
-            ]
-        ],
-        True,
-    )
+_LOGGER = logging.getLogger(__name__)
 
 
-class TeslaBinarySensor(TeslaDevice, BinarySensorEntity):
-    """Implement an Tesla binary sensor for parking and charger."""
+async def async_setup_entry(hass: HomeAssistant, config_entry, async_add_entities):
+    """Set up the Tesla selects by config_entry."""
+    coordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
+    cars = hass.data[DOMAIN][config_entry.entry_id]["cars"]
+    energysites = hass.data[DOMAIN][config_entry.entry_id]["energysites"]
+    entities = []
+
+    for car in cars.values():
+        entities.append(TeslaCarParkingBrake(hass, car, coordinator))
+        entities.append(TeslaCarOnline(hass, car, coordinator))
+        entities.append(TeslaCarAsleep(hass, car, coordinator))
+        entities.append(TeslaCarChargerConnection(hass, car, coordinator))
+        entities.append(TeslaCarCharging(hass, car, coordinator))
+        entities.append(TeslaCarDoors(hass, car, coordinator))
+
+    for energysite in energysites.values():
+        if energysite.resource_type == RESOURCE_TYPE_BATTERY:
+            entities.append(TeslaEnergyBatteryCharging(hass, energysite, coordinator))
+            entities.append(TeslaEnergyGridStatus(hass, energysite, coordinator))
+
+    async_add_entities(entities, True)
+
+
+class TeslaCarParkingBrake(TeslaCarEntity, BinarySensorEntity):
+    """Representation of a Tesla car parking brake binary sensor."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        car: TeslaCar,
+        coordinator: TeslaDataUpdateCoordinator,
+    ) -> None:
+        """Initialize parking brake entity."""
+        super().__init__(hass, car, coordinator)
+        self.type = "parking brake"
+        self._attr_icon = "mdi:car-brake-parking"
+        self._attr_device_class = None
 
     @property
-    def device_class(self):
-        """Return the class of this binary sensor."""
-        return (
-            self.tesla_device.sensor_type
-            if self.tesla_device.sensor_type in DEVICE_CLASSES
-            else None
-        )
+    def is_on(self):
+        """Return True if car shift state in park or None."""
+        # When car is parked and off, Tesla API reports shift_state None
+        return self._car.shift_state == "P" or self._car.shift_state is None
+
+
+class TeslaCarChargerConnection(TeslaCarEntity, BinarySensorEntity):
+    """Representation of a Tesla car charger connection binary sensor."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        car: TeslaCar,
+        coordinator: TeslaDataUpdateCoordinator,
+    ) -> None:
+        """Initialize charger connection entity."""
+        super().__init__(hass, car, coordinator)
+        self.type = "charger"
+        self._attr_icon = "mdi:ev-station"
+        self._attr_device_class = BinarySensorDeviceClass.PLUG
+
+    @property
+    def is_on(self):
+        """Return True if charger connected."""
+        return self._car.charging_state != "Disconnected"
+
+    @property
+    def extra_state_attributes(self):
+        """Return device state attributes."""
+        return {
+            "charging_state": self._car.charging_state,
+            "conn_charge_cable": self._car.conn_charge_cable,
+            "fast_charger_present": self._car.fast_charger_present,
+            "fast_charger_brand": self._car.fast_charger_brand,
+            "fast_charger_type": self._car.fast_charger_type,
+        }
+
+
+class TeslaCarCharging(TeslaCarEntity, BinarySensorEntity):
+    """Representation of Tesla car charging binary sensor."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        car: TeslaCar,
+        coordinator: TeslaDataUpdateCoordinator,
+    ) -> None:
+        """Initialize charging entity."""
+        super().__init__(hass, car, coordinator)
+        self.type = "charging"
+        self._attr_icon = "mdi:ev-station"
+        self._attr_device_class = BinarySensorDeviceClass.BATTERY_CHARGING
 
     @property
     def is_on(self):
         """Return the state of the binary sensor."""
-        return self.tesla_device.get_value()
+        return self._car.charging_state == "Charging"
+
+
+class TeslaCarOnline(TeslaCarEntity, BinarySensorEntity):
+    """Representation of a Tesla car online binary sensor."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        car: TeslaCar,
+        coordinator: TeslaDataUpdateCoordinator,
+    ) -> None:
+        """Initialize car online entity."""
+        super().__init__(hass, car, coordinator)
+        self.type = "online"
+        self._attr_device_class = BinarySensorDeviceClass.CONNECTIVITY
+
+    @property
+    def is_on(self):
+        """Return True if car is online."""
+        return self._car.is_on
+
+    @property
+    def extra_state_attributes(self):
+        """Return device state attributes."""
+        return {
+            "vehicle_id": str(self._car.vehicle_id),
+            "vin": self._car.vin,
+            "id": str(self._car.id),
+            "state": self._car.state,
+        }
+
+
+class TeslaCarAsleep(TeslaCarEntity, BinarySensorEntity):
+    """Representation of a Tesla car asleep binary sensor."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        car: TeslaCar,
+        coordinator: TeslaDataUpdateCoordinator,
+    ) -> None:
+        """Initialize car asleep entity."""
+        super().__init__(hass, car, coordinator)
+        self.type = "asleep"
+        self._attr_device_class = None
+        self._attr_icon = "mdi:sleep"
+
+    @property
+    def is_on(self):
+        """Return True if car is asleep."""
+        return self._car.state == "asleep"
+
+
+class TeslaEnergyBatteryCharging(TeslaEnergyEntity, BinarySensorEntity):
+    """Representation of a Tesla energy charging binary sensor."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        energysite: PowerwallSite,
+        coordinator: TeslaDataUpdateCoordinator,
+    ) -> None:
+        """Initialize battery charging entity."""
+        super().__init__(hass, energysite, coordinator)
+        self.type = "battery charging"
+        self._attr_device_class = BinarySensorDeviceClass.BATTERY_CHARGING
+        self._attr_icon = "mdi:battery-charging"
+
+    @property
+    def is_on(self) -> bool:
+        """Return True if battery charging."""
+        return self._energysite.battery_power < -100
+
+
+class TeslaEnergyGridStatus(TeslaEnergyEntity, BinarySensorEntity):
+    """Representation of the Tesla energy grid status binary sensor."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        energysite: PowerwallSite,
+        coordinator: TeslaDataUpdateCoordinator,
+    ) -> None:
+        """Initialize grid status entity."""
+        super().__init__(hass, energysite, coordinator)
+        self.type = "grid status"
+        self._attr_device_class = BinarySensorDeviceClass.POWER
+
+    @property
+    def is_on(self) -> bool:
+        """Return True if grid status is active."""
+        return self._energysite.grid_status == GRID_ACTIVE
+
+
+class TeslaCarDoors(TeslaCarEntity, BinarySensorEntity):
+    """Representation of a Tesla car door sensor."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        car: TeslaCar,
+        coordinator: TeslaDataUpdateCoordinator,
+    ) -> None:
+        """Initialize car door entity."""
+        super().__init__(hass, car, coordinator)
+        self.type = "doors"
+        self._attr_device_class = BinarySensorDeviceClass.DOOR
+        self._attr_icon = "mdi:car-door"
+
+    @property
+    def is_on(self):
+        """Return True if a car door is open."""
+        return self._car.door_df or self._car.door_dr or self._car.door_pf or self._car.door_pr
+
+    @property
+    def extra_state_attributes(self):
+        """Return device state attributes."""
+        return {
+            "Driver Front": self._open_or_closed(self._car.door_df),
+            "Driver Rear": self._open_or_closed(self._car.door_dr),
+            "Passenger Front": self._open_or_closed(self._car.door_pf),
+            "Passenger Rear": self._open_or_closed(self._car.door_pr),
+        }
+
+    def _open_or_closed(self, door):
+        """Return string of 'Open' or 'Closed' when passed a door integer state."""
+        if door:
+            return "Open"
+        return "Closed"
--- custom_components/tesla_custom/lock.py
+++ custom_components/tesla_custom/lock.py
@@ -1,44 +1,99 @@
-"""Support for Tesla door locks."""
+"""Support for Tesla locks."""
 import logging
 
-from homeassistant.components.lock import LockEntity
+from teslajsonpy.car import TeslaCar
 
-from . import DOMAIN as TESLA_DOMAIN
-from .tesla_device import TeslaDevice
+from homeassistant.components.lock import (
+    LockEntity,
+    LockEntityFeature,
+)
+from homeassistant.core import HomeAssistant
+
+from . import TeslaDataUpdateCoordinator
+from .base import TeslaCarEntity
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up the Tesla binary_sensors by config_entry."""
-    entities = [
-        TeslaLock(
-            device,
-            hass.data[TESLA_DOMAIN][config_entry.entry_id]["coordinator"],
-        )
-        for device in hass.data[TESLA_DOMAIN][config_entry.entry_id]["devices"]["lock"]
-    ]
+async def async_setup_entry(hass: HomeAssistant, config_entry, async_add_entities):
+    """Set up the Tesla locks by config_entry."""
+    coordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
+    cars = hass.data[DOMAIN][config_entry.entry_id]["cars"]
+    entities = []
+
+    for car in cars.values():
+        entities.append(TeslaCarDoors(hass, car, coordinator))
+        entities.append(TeslaCarChargePortLatch(hass, car, coordinator))
+
     async_add_entities(entities, True)
 
 
-class TeslaLock(TeslaDevice, LockEntity):
-    """Representation of a Tesla door lock."""
+class TeslaCarDoors(TeslaCarEntity, LockEntity):
+    """Representation of a Tesla car door lock."""
 
-    @TeslaDevice.Decorators.check_for_reauth
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        car: TeslaCar,
+        coordinator: TeslaDataUpdateCoordinator,
+    ) -> None:
+        """Initialize door lock entity."""
+        super().__init__(hass, car, coordinator)
+        self.type = "doors"
+
     async def async_lock(self, **kwargs):
-        """Send the lock command."""
-        _LOGGER.debug("Locking doors for: %s", self.name)
-        await self.tesla_device.lock()
-        self.async_write_ha_state()
+        """Send lock command."""
+        _LOGGER.debug("Locking: %s", self.name)
+        await self._car.lock()
+        await self.async_update_ha_state()
 
-    @TeslaDevice.Decorators.check_for_reauth
     async def async_unlock(self, **kwargs):
-        """Send the unlock command."""
-        _LOGGER.debug("Unlocking doors for: %s", self.name)
-        await self.tesla_device.unlock()
-        self.async_write_ha_state()
+        """Send unlock command."""
+        _LOGGER.debug("Unlocking: %s", self.name)
+        await self._car.unlock()
+        await self.async_update_ha_state()
 
     @property
     def is_locked(self):
-        """Get whether the lock is in locked state."""
-        return self.tesla_device.is_locked()
+        """Return True if door is locked."""
+        return self._car.is_locked
+
+
+class TeslaCarChargePortLatch(TeslaCarEntity, LockEntity):
+    """Representation of a Tesla charge port latch."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        car: TeslaCar,
+        coordinator: TeslaDataUpdateCoordinator,
+    ) -> None:
+        """Initialize charge port latch (lock) entity."""
+        super().__init__(hass, car, coordinator)
+        self.type = "charge port latch"
+        self._attr_icon = "mdi:ev-plug-tesla"
+        self._attr_supported_features = LockEntityFeature.OPEN
+
+    async def async_open(self, **kwargs):
+        """Send open command."""
+        _LOGGER.debug("Opening: %s", self.name)
+        await self._car.charge_port_door_open()
+        await self.async_update_ha_state()
+
+    async def async_unlock(self, **kwargs):
+        """Send unlock command."""
+        _LOGGER.debug("Unlocking: %s", self.name)
+        await self._car.charge_port_door_open()
+        await self.async_update_ha_state()
+
+    async def async_lock(self, **kwargs):
+        """Log lock command not possible."""
+        _LOGGER.debug("Locking charge port latch not possible with Tesla's API.")
+
+    @property
+    def is_locked(self):
+        """Return True if charge port latch is engaged."""
+        if self._car.charge_port_latch == "Engaged":
+            return True
+        return False
